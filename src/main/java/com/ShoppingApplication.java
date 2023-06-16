a9791e509f924d97e5da82bef5fc8585016cf9c8
iamkang02 <iamkang02@i-screamedu.co.kr>
2023-06-16 17:13:43 +0900

    war 톰캣 설정

--- src/main/java/com/ShoppingApplication.java
+++ src/main/java/com/ShoppingApplication.java
@@ -2,10 +2,16 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShoppingApplication {
+public class ShoppingApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ShoppingApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingApplication.class, args);
 	}
